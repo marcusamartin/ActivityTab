2a2414f3d49a253d68a4a238099263aa2748de98
marcusamartin <33076237+marcusamartin@users.noreply.github.com>
2018-09-30 08:36:24 -0700

    Changed storage to sync instead of local
    This allows Chrome's storage to be accessible by multiple computers and allow the extension information to be the same across multiple computers.

--- popup.js
+++ popup.js
@@ -4,7 +4,7 @@ displayButtons();
 /* Display the buttons in the popup */
 function displayButtons()
 {
-	chrome.storage.local.get("objectArr", function(group)
+	chrome.storage.sync.get("objectArr", function(group)
 	{
 		var objectArr = group.objectArr;
 		
@@ -186,10 +186,10 @@ function displayTrashButton(group, i)
 			// remove the object at the specific index
 			objectArr.splice(i, 1);
 
-			chrome.storage.local.set({"objectArr": objectArr});
+			chrome.storage.sync.set({"objectArr": objectArr});
 
 			/* set empty text if applicable */
-			chrome.storage.local.get("objectArr", function(group)
+			chrome.storage.sync.get("objectArr", function(group)
 			{
 				if (group.objectArr.length == 0)
 				{
